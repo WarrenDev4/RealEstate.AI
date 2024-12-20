2df42dc461f818b249924528e49eaa03c1b86207
Warrenn205 <warrenspencer41@gmail.com>
2024-12-20 02:07:06 -0600

    Update

--- my-react-app/src/components/LandingPageNav.jsx
+++ my-react-app/src/components/LandingPageNav.jsx
@@ -4,25 +4,11 @@ import "./LandingPageNav.css";
 
 const LandingPageNav = () => {
 
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-
   return (
     <nav className="landingnavbar">
       <a href="/" className="landingnavbar-brand">
         <img src="/images/RealEstate.AI Logo Black.png" alt="RealEsate.AI Black" class="realestateblack" height="55" width="245"></img>
       </a>
-      <div className={`landingnavbar-links ${menuOpen ? "active" : ""}`}>
-        <a href="/sign-up">Sign Up</a>
-        <a href="/login">Login</a>
-      </div>
-      <div className="menu-btn" onClick={toggleMenu}>
-        ☰
-      </div>
     </nav>
   );
 };
