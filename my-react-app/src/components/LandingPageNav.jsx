fe8008679b32f30ec856206b5c8d35262db9a5f6
Warrenn205 <warrenspencer41@gmail.com>
2024-12-20 02:45:08 -0600

    Working on the Landing Page

--- my-react-app/src/components/LandingPageNav.jsx
+++ my-react-app/src/components/LandingPageNav.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
 import "./LandingPageNav.css";
 
 const LandingPageNav = () => {
-
   return (
     <nav className="landingnavbar">
       <a href="/" className="landingnavbar-brand">
-        <img src="/images/RealEstate.AI Logo Black.png" alt="RealEsate.AI Black" class="realestateblack" height="55" width="245"></img>
+        <img
+          src="/images/RealEstate.AI Logo Black.png"
+          alt="RealEstate.AI Black"
+          className="realestateblack"
+          height="55"
+          width="245"
+        />
       </a>
     </nav>
   );
