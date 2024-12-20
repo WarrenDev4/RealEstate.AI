baf68216520fc115cd0a395c3e96c6dc738b7660
Warrenn205 <warrenspencer41@gmail.com>
2024-12-20 02:54:27 -0600

    Update

--- my-react-app/src/components/LandingPageNav.jsx
+++ my-react-app/src/components/LandingPageNav.jsx
@@ -5,13 +5,8 @@ const LandingPageNav = () => {
   return (
     <nav className="landingnavbar">
       <a href="/" className="landingnavbar-brand">
-        <img
-          src="/images/RealEstate.AI Logo Black.png"
-          alt="RealEstate.AI Black"
-          className="realestateblack"
-          height="55"
-          width="245"
-        />
+        <img src="/images/RealEstate.AI Logo Black.png" alt="RealEstate.AI Black" className="realestateblack"
+          height="55" width="245"/>
       </a>
     </nav>
   );
