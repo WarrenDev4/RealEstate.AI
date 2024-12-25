39b9e446306e9569b2a516b95749a229a38c712e
Warrenn205 <warrenspencer41@gmail.com>
2024-12-24 20:06:14 -0600

    Creating Landing Page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Trends from './pages/Trends';
-import Properties from './pages/Properties';
-import Neighborhoods from './pages/Neighborhoods';
-import Investments from './pages/Investments';
-import logo from './logo.svg';
 import './App.css';
-import LandingPageNav from './components/LandingPageNav';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <Router>
-      <LandingPageNav />
+      <MainContent />
     </Router>
   );
 };
