337c85b27aca9d9abca823c24fa8b69055c46d15
Warrenn205 <warrenspencer41@gmail.com>
2024-12-25 02:19:23 -0600

    Created Landing Page

--- my-react-app/src/components/MainContent.jsx
+++ my-react-app/src/components/MainContent.jsx
@@ -4,16 +4,20 @@ import './MainContent.css';
 const MainContent = () => {
   return (
     <section className="main-content">
-        
       <img src="/images/Landing Page Background_enhanced.jpg" alt="Background" className="background" />
-      <img src="/images/RealEstate.AI Logo Black.png" href="/" alt="RealEstate.AI Black" className="realestate-logo" />
+      <a href="/">
+        <img src="/images/RealEstate.AI Logo Black.png" alt="RealEstate.AI Black" className="realestate-logo" />
+        <a href="#login" className="login-button">Log In</a>
+      </a>
+
       <div className="content-wrapper">
         <div className="text-section">
           <h2 className="headline">Welcome to RealEstate.AI</h2>
-          <p className="description"> RealEstate.AI is an AI-driven tool designed to analyze real estate market data that provides 
+          <p className="description">
+            RealEstate.AI is an AI-driven real estate tool designed to analyze data that provides
             insights into market trends, property valuations, neighborhood analysis, investment forecasting, and comparable sales data.
           </p>
-          <a href="#cta" className="cta-button">Register Here</a>
+          <a href="#register" className="register-button">Register Here</a>
         </div>
       </div>
     </section>
@@ -21,3 +25,4 @@ const MainContent = () => {
 };
 
 export default MainContent;
+
---
+++ my-react-app/src/components/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Register = () => {
+    return (
+        <div className="register-container">
+            <a href="#register" className="register-button">Register Here</a>
+        </div>
+    );
+};
+
+export default Register;
