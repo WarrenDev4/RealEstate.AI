7c4df6a441acc832bc9b7aa57d49c39014a9d376
Warrenn205 <warrenspencer41@gmail.com>
2025-01-06 06:59:47 -0600

    Updated Landing Page

--- my-react-app/src/components/MainContent.jsx
+++ my-react-app/src/components/MainContent.jsx
@@ -3,24 +3,27 @@ import './MainContent.css';
 
 const MainContent = () => {
   return (
-    <section className="main-content">
-      <img src="/images/Landing Page Background_enhanced.jpg" alt="Background" className="background" />
-      <a href="/">
-        <img src="/images/RealEstate.AI Logo Black.png" alt="RealEstate.AI Black" className="realestate-logo" />
-        <a href="#login" className="login-button">Log In</a>
-      </a>
+    <section class="main-content">
+  <img src="/images/Landing Page Background_enhanced.jpg" alt="Background" class="background" />
+  
+  <div class="header">
+    <a href="/">
+      <img src="/images/RealEstate.AI Logo Black.png" alt="RealEstate.AI Black" class="realestate-logo" />
+    </a>
+    <a href="#login" class="login-button">Log In</a>
+  </div>
 
-      <div className="content-wrapper">
-        <div className="text-section">
-          <h2 className="headline">Welcome to RealEstate.AI</h2>
-          <p className="description">
-            RealEstate.AI is an AI-driven real estate tool designed to analyze data that provides
-            insights into market trends, property valuations, neighborhood analysis, investment forecasting, and comparable sales data.
-          </p>
-          <a href="#register" className="register-button">Register Here</a>
-        </div>
-      </div>
-    </section>
+  <div class="content-wrapper">
+    <div class="text-section">
+      <h2 class="headline">Welcome to RealEstate.AI</h2>
+      <p class="description">
+        RealEstate.AI is an AI-driven real estate tool designed to analyze data that provides
+        insights into market trends, property valuations, neighborhood analysis, investment forecasting, and comparable sales data.
+      </p>
+      <a href="#register" class="register-button">Register Here</a>
+    </div>
+  </div>
+</section>
   );
 };
 
