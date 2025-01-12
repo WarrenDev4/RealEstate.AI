383edbb98ecbc6f27dfe205fc89cb66f09e9e7d9
Warrenn205 <warrenspencer41@gmail.com>
2025-01-12 01:30:54 -0600

    Update

--- realestate.ai/src/App.js
+++ realestate.ai/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import MainContent from './components/LandingPage/MainContent';
+import MainContent from './pages/LandingPage/MainContent';
 
 function App() {
   return (
