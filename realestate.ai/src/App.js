82a121201dff6b6b11e73ff13697aa4410f45415
Warrenn205 <warrenspencer41@gmail.com>
2025-02-02 21:32:50 -0600

    Created Log In Page

--- realestate.ai/src/App.js
+++ realestate.ai/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainContent from './pages/LandingPage/MainContent';
 import Register from './pages/RegisterPage/Register'; 
+import Login from './pages/LoginPage/Login'; 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainContent />} />
-        <Route path="/register" element={<Register />} />  
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/login" element={<Login />} /> 
       </Routes>
     </Router>
   );
