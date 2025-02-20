13b7619f694b57d8f90128c167889a4071efc989
Warrenn205 <warrenspencer41@gmail.com>
2025-02-20 13:34:59 -0600

    Creating MySQL Functionality

---
+++ src/main/java/com/warren/real_estate_ai/controller/Controller.java
@@ -0,0 +1,33 @@
+package com.warren.real_estate_ai.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.warren.real_estate_ai.model.User;
+import com.warren.real_estate_ai.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class Controller {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody User user){
+        userService.saveUser(user);
+        return "New user is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<User> list(){
+        return userService.getAllUsers();
+    }
+}
---
+++ src/main/java/com/warren/real_estate_ai/model/User.java
@@ -0,0 +1,60 @@
+package com.warren.real_estate_ai.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public String getpassword() {
+        return password;
+    }
+
+    public void setpassword(String password) {
+        this.password = password;
+    }
+}
