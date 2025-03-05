a17d6e1fed30f5a7dc2c22f3bb234917c8c0805e
Warrenn205 <warrenspencer41@gmail.com>
2025-03-05 02:22:07 -0600

    Update

--- realestate.ai/src/components/Navbar/Navbar.jsx
+++ realestate.ai/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaUserCircle, FaHome, FaChartLine, FaBuilding, FaBullseye, FaBell } from 'react-icons/fa'; // Import icons
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  FaUserCircle,
+  FaHome,
+  FaChartLine,
+  FaBuilding,
+  FaBullseye,
+  FaBell,
+} from "react-icons/fa";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,27 +20,33 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-brand">
-        <img src="/images/RealEstate.AI Logo White.png" alt="RealEstate.AI Logo" className="realestatelogo" height="50" width="210" />
+        <img
+          src="/images/RealEstate.AI Logo White.png"
+          alt="RealEstate.AI Logo"
+          className="realestatelogo"
+          height="50"
+          width="210"
+        />
       </Link>
 
-      <div className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+      <div className={`navbar-links ${menuOpen ? "active" : ""}`}>
         <Link to="/" className="navbar-link">
-          <FaHome size={20} style={{ marginRight: '5px', marginTop: '7px' }} /> Home
+          <FaHome className="icon" /> Home
         </Link>
         <Link to="/market-trends" className="navbar-link">
-          <FaChartLine size={20} style={{ marginRight: '5px', marginTop: '7px' }} /> Trends
+          <FaChartLine className="icon" /> Trends
         </Link>
         <Link to="/property-valuations" className="navbar-link">
-          <FaBuilding size={20} style={{ marginRight: '5px', marginTop: '7px' }} /> Properties
+          <FaBuilding className="icon" /> Properties
         </Link>
         <Link to="/forecasting" className="navbar-link">
-          <FaBullseye size={20} style={{ marginRight: '5px', marginTop: '7px' }} /> Forecasting
+          <FaBullseye className="icon" /> Forecasting
         </Link>
       </div>
 
       <div className="navbar-right">
         <Link to="/notifications" className="notification-icon">
-          <FaBell size={24} style={{ marginRight: '10px', cursor: 'pointer', color: 'white' }} />
+          <FaBell size={22} />
         </Link>
         <Link to="/profile" className="profile-icon">
           <FaUserCircle size={30} />
