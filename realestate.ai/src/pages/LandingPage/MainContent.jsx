cc7c1a2961a13b5bcde725afaf6a76a599cef703
Warrenn205 <warrenspencer41@gmail.com>
2026-03-23 06:23:59 -0500

    Update

--- realestate.ai/src/pages/LandingPage/MainContent.jsx
+++ realestate.ai/src/pages/LandingPage/MainContent.jsx
@@ -20,8 +20,12 @@ const MainContent = () => {
     <div class="text-section">
       <h2 class="headline">Welcome to RealEstate.AI</h2>
       <p class="description">
-        RealEstate.AI is an AI-driven real estate tool designed to analyze data that provides
-        insights into market trends, neighborhood analysis, investment forecasting, and comparable sales data.
+        RealEstate.AI was supposed to be an AI-driven real estate tool designed to analyze data that provides
+        insights into market trends, neighborhood analysis, investment forecasting, and comparable sales data. However, due to 
+        unforseen circumstances over the past year I wasn't able do all the other work I wanted to do on it. But I did alot of 
+        frontend and UI work with React.js. So please browse the application to view the frontend work I did for it!
+
+        - Warren
       </p>
       <LoginButton />
     </div>
